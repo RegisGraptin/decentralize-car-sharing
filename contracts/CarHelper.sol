1fe10c5759ccb6611336d4c9f4eb713057feab63
rere <regis-graptin@sfr.fr>
2022-06-20 20:46:25 +0200

    feat: get list of car from the owner

---
+++ contracts/CarHelper.sol
@@ -0,0 +1,20 @@
+
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./CarFactory.sol";
+
+contract CarHelper is CarFactory {
+
+    function getCarByOwner(address _owner) external view returns(uint[] memory) {
+        uint[] memory carList = new uint[](ownerCarCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < cars.length; i++) {
+            if (carToOwner[i] == _owner) {
+                carList[counter] = i;
+                counter++;
+            }
+        }
+        return carList;
+    }
+
+}
